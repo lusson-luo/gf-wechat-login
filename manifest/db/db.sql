baa7e8bee59301cbc4097fe13d7be76e0c38672c
lusson <luocheng@coding.net>
2023-06-17 14:57:26 +0800

    微信登录功能

--- manifest/db/db.sql
+++ manifest/db/db.sql
@@ -8,4 +8,20 @@ CREATE TABLE IF NOT EXISTS user(
     update_at datetime(0) DEFAULT NULL --  'Updated Time'
 );
 
-INSERT INTO user(passport, password, nickname, create_at, update_at) VALUES ('admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', '管理员', datetime('now'), datetime('now'));
+REPLACE INTO user
+(id, passport, password, nickname, create_at, update_at) 
+VALUES 
+(1, 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', '管理员', datetime('now'), datetime('now')),
+(2, 'guest', '84983c60f7daadc1cb8698621f802c0d9f9a3c3c295c810748fb048115c186ec', '访客', datetime('now'), datetime('now'));
+
+-- DROP TABLE IF EXISTS wx_user;
+CREATE TABLE IF NOT EXISTS wx_user(
+    user_id INTEGER PRIMARY KEY,  -- 'userID'
+    open_id  varchar(45) NOT NULL, --  '余额'
+    phone_no  varchar(45) DEFAULT NULL, --  '手机号'
+    avatar_url  varchar(45) DEFAULT NULL, --  '头像地址'
+    nickname  varchar(45) DEFAULT NULL, --  '昵称'
+    gender  varchar(45) DEFAULT NULL, --  '性别'
+    create_at datetime(0) DEFAULT NULL, --  'Created Time'
+    update_at datetime(0) DEFAULT NULL --  'Updated Time'
+);
